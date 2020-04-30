07530510f6cf431ea1249f3f27ca90a22e2c4cc0
Martin <martin@caarels.com>
2020-04-30 22:31:56 +1200

    cleaned up response functions

--- index.js
+++ index.js
@@ -10,40 +10,38 @@ function createJSONResponse(requestObject) {
 	response.Host    = requestObject.host
 	response.Path    = requestObject.path
 	response.Queries = requestObject.query
-	response.data    = requestObject.body
+	response.Data    = requestObject.body
 	return response
 }
 
 function createHTMLResponse(requestObject) {
 	let response = ""
-	response = response + '<h4>Method:</h4>\n'
-	response = response + requestObject.method + '\n'
-	response = response + '<h4>Headers:</h4>\n'
+	response += '<h4>Method:</h4>\n'
+	response += requestObject.method + '\n'
+	response += '<h4>Headers:</h4>\n'
 	for (header in requestObject.headers){
-		response = 	response + 
-								'<b>' +
+		response += '<b>' +
 								header +
 								'</b>' +
 								' = ' +
 								requestObject.headers[header] + 
 								'<br>\n'
 	}
-	response = response + '<h4>Host:</h4>\n'
-	response = response + requestObject.host + '\n'
-	response = response + '<h4>Path:</h4>\n'
-	response = response + requestObject.path + '\n'
-	response = response + '<h4>Queries:</h4>\n'
+	response += '<h4>Host:</h4>\n'
+	response += requestObject.host + '\n'
+	response += '<h4>Path:</h4>\n'
+	response += requestObject.path + '\n'
+	response += '<h4>Queries:</h4>\n'
 	for (query in requestObject.query){
-		response = 	response + 
-								'<b>' +
+		response += '<b>' +
 								query +
 								'</b>' +
 								' = ' +
 								requestObject.query[query] + 
 								'<br>\n'
 	}
-	response = response + '<h4>Data:</h4>\n'
-	response = response + JSON.stringify(requestObject.body) + '\n'
+	response += '<h4>Data:</h4>\n'
+	response += JSON.stringify(requestObject.body) + '\n'
 	return response
 }
 
